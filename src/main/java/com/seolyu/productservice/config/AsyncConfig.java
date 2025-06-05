3c1b13096eb2a5b35906839ca23ca8cd85850741
김진성 <wlsdiqkdrk@naver.com>
2025-06-05 14:33:52 +0900

    feature: config 개발

---
+++ src/main/java/com/seolyu/productservice/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.seolyu.productservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
